7c9e1e2bfddd74119ac948bcb8746d9db808bb82
AniruDra <magarrudra999@gmail.com>
2022-12-11 09:05:54 +0545

    Nice try

--- MainMenu.cs
+++ MainMenu.cs
@@ -16,7 +16,7 @@ namespace CsharpForm
         {
             InitializeComponent();
         }
-        //Try
+        //Try - Nice try
         //Main Menu main Form
         private void MainMenu_Load(object sender, EventArgs e)
         {
